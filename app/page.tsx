cb44203bfa2fa07541c4684f55b71904f5f1400f
Ondrej Sevcik <ondrej.sevcik@oriflame.com>
2024-05-28 17:57:01 +0200

    added facebook

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import FacebookIcon from '@/components/icons/facebook';
 import Typography from '@/components/typography';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { InstagramLogoIcon } from '@radix-ui/react-icons';
 import { Playfair_Display, Poppins } from 'next/font/google';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const poppins = Poppins({ weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], subsets: ['latin'] });
 const playfairDisplay = Playfair_Display({
@@ -26,10 +28,16 @@ export default function Home() {
         Soon!
       </Typography>
       <Typography className="text-2xl">We are currently making some improvments to the website!</Typography>
-      <Button>Notify me</Button>
       <div className="flex gap-4">
         <Button variant="ghost" className="p-2">
-          <InstagramLogoIcon className="w-8 h-8" />
+          <Link href="https://www.instagram.com/balance_glow_studio" className="w-8 h-8" target="_blank">
+            <InstagramLogoIcon />
+          </Link>
+        </Button>
+        <Button variant="ghost" className="p-2">
+          <Link href="https://www.facebook.com/share/UCoQMtkUQfmBvqex/?mibextid=LQQJ4d" className="w-8 h-8" target="_blank">
+            <FacebookIcon />
+          </Link>
         </Button>
       </div>
       <Image src="/bg.jpg" alt="Background" className="absolute top-0 left-0 object-cover h-full w-full -z-10" width={1232} height={928} />
