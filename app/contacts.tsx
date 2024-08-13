ce4300052ecc07aaae7696fc66009001ed336b7d
Ondrej Sevcik <o.sevcik91@gmail.com>
2024-08-13 18:13:37 +0200

    update

--- app/contacts.tsx
+++ app/contacts.tsx
@@ -17,14 +17,7 @@ const ContactValue = ({ title, children }: { title: string; children: React.Reac
 
 const Contacts = ({ secondaryBg = false }: { secondaryBg?: boolean }) => {
   return (
-    <Section className={`${secondaryBg ? 'bg-secondary' : 'bg-ternary'} grid lg:grid-cols-[4fr_3fr] gap-6`} padding="lg" id="Kontakty">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2573.169841540513!2d18.283089776966044!3d49.83926617148197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4713e324db50818b%3A0x633f7cd920095dfd!2sVeleslav%C3%ADnova%204%2C%20702%2000%20Moravsk%C3%A1%20Ostrava%20a%20P%C5%99%C3%ADvoz!5e0!3m2!1sen!2scz!4v1722436475006!5m2!1sen!2scz"
-        allowFullScreen
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-        className="w-full h-[400px] lg:h-full"
-      ></iframe>
+    <Section className={`${secondaryBg ? 'bg-secondary' : 'bg-ternary'} grid lg:grid-cols-[4fr_5fr] gap-6 `} padding="lg" id="Kontakty">
       <div className="flex flex-col h-full gap-4 uppercase text-lg tracking-tighter font-thin">
         <Typography variant="h1" className="uppercase ">
           Kontakty
@@ -39,12 +32,34 @@ const Contacts = ({ secondaryBg = false }: { secondaryBg?: boolean }) => {
           <ContactValue title="Adresa">Veleslavínová 4, Ostrava 📍</ContactValue>
           <ContactValue title="Bankovní spojení">11122233/0800 </ContactValue>
         </div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2573.169841540513!2d18.283089776966044!3d49.83926617148197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4713e324db50818b%3A0x633f7cd920095dfd!2sVeleslav%C3%ADnova%204%2C%20702%2000%20Moravsk%C3%A1%20Ostrava%20a%20P%C5%99%C3%ADvoz!5e0!3m2!1sen!2scz!4v1722436475006!5m2!1sen!2scz"
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          className="w-full h-[400px] lg:h-full"
+        ></iframe>
+      </div>
+      <div className="flex flex-col mx-auto h-full w-3/4 gap-4 uppercase text-lg tracking-tighter font-thin items-center justify-center">
+        {/* <Typography variant="h1" className="uppercase ">
+          Kontakty
+        </Typography>
+        <div className="flex flex-col gap-1 ">
+          <ContactValue title="Email recepce">
+            <a href="mailto:info@studiobg.cz">info@studiobg.cz</a>
+          </ContactValue>
+          <ContactValue title="Telefon">
+            <a href="tel:+420736129088">736 129 088</a>
+          </ContactValue>
+          <ContactValue title="Adresa">Veleslavínová 4, Ostrava 📍</ContactValue>
+          <ContactValue title="Bankovní spojení">11122233/0800 </ContactValue>
+        </div> */}
 
-        <form action={sendContactForm} className="flex gap-1 flex-col flex-grow bg-orange-50 p-8 shadow-md border border-gray-200 h-full">
+        <form action={sendContactForm} className="flex gap-1 flex-col w-3/4 bg-orange-50 p-8 shadow-md border border-gray-200 h-1/2">
           <Typography>Kontaktujte nás</Typography>
           <Input type="text" name="name" required placeholder="Jméno:" className="border-none shadow-none text-xl placeholder:text-xl" />
           <Input type="email" name="email" required placeholder="Email:" className="border-none shadow-none text-xl placeholder:text-xl" />
-          <Textarea name="message" required placeholder="Zpráva:" className="border-none shadow-none text-xl placeholder:text-xl resize-none h-full" />
+          <Textarea name="message" required placeholder="Zpráva:" className="border-none shadow-none text-xl placeholder:text-xl resize-none h-1/2" />
           <FormSubmitButton className="text-primary">Odeslat</FormSubmitButton>
         </form>
       </div>
